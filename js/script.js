c7a7c70cc4a05dd4a0ac389e40c1518c33ca6c92
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 08:30:35 -0400

    9/11/2014 updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("This is in the js file");
+//Prompt and Alert lesson.
+var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height; // calculates the specified info.
+//console.log(area); //prints out to the console
+var result = "The area of the rectangle is " + area + "square ft.";
+alert(result);
